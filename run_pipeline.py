1d45e4048c25962751d9105d06e19ff52327702b
ArturoBarrios <arturobarrios0714@gmail.com>
2025-06-11 20:15:56 -0400

    trigger audio

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,10 @@
 from capture import detect_motion_and_capture
 from recognize import recognize_plate
 import time
+import requests
+import os
+
+AUDIO_URL = os.environ.get("AUDIO_URL", "localhost:6000")
 
 def main():
     print("🚘 Drive-thru plate reader with motion detection ready...")
@@ -15,6 +19,14 @@ def main():
         if result:
             print(f"[SUCCESS] Plate Detected: {result['plate']} (score: {result['score']})")
             print(f"Bounding box: {result['box']}")
+            try:
+                url = f"http://{AUDIO_URL}/trigger-audio"
+                response = requests.post(url, json=result)
+                print(f"[AUDIO SERVER] Status: {response.status_code}")
+                print(f"[AUDIO SERVER] Response: {response.text}")
+            except requests.exceptions.RequestException as e:
+                print(f"[ERROR] Could not reach audio server: {e}")
+            
         else:
             print("[FAILURE] No plate detected.")
 
